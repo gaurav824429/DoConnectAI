4e1985eaac7a0e5527026d058839898176dbc609
Gaurav Rathod <gaurav2021rathod@gmail.com>
2026-05-30 17:40:55 +0530

    Linked questions with authenticated users

--- backend/src/main/java/com/doconnect/doconnectai/service/QuestionService.java
+++ backend/src/main/java/com/doconnect/doconnectai/service/QuestionService.java
@@ -2,11 +2,15 @@ package com.doconnect.doconnectai.service;
 
 import java.time.LocalDateTime;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import com.doconnect.doconnectai.dto.QuestionRequest;
 import com.doconnect.doconnectai.entity.Question;
+import com.doconnect.doconnectai.entity.User;
 import com.doconnect.doconnectai.repository.QuestionRepository;
+import com.doconnect.doconnectai.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -15,14 +19,27 @@ import lombok.RequiredArgsConstructor;
 public class QuestionService {
 
     private final QuestionRepository questionRepository;
+    private final UserRepository userRepository;
 
     public String createQuestion(
             QuestionRequest request) {
+
+        Authentication authentication =
+                SecurityContextHolder.getContext()
+                        .getAuthentication();
+
+        String email = authentication.getName();
+
+        User user = userRepository
+                .findByEmail(email)
+                .orElseThrow(() ->
+                        new RuntimeException("User not found"));
 
         Question question = Question.builder()
                 .title(request.getTitle())
                 .description(request.getDescription())
                 .createdAt(LocalDateTime.now())
+                .user(user)
                 .build();
 
         questionRepository.save(question);
